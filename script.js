ddbc5291657e6b1f47920bb75c1d86bdbea1fa90
richmondmpienga-crypto <richmond.mpienga@gmail.com>
2026-09-17 16:59:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -805,7 +805,7 @@ cells[8].textContent = analysis.signal;
         cells[8].style.color = "#ff4d4d";
       } else {
         cells[8].style.color = "#ffb000";
-      }
+      
 if (cells[9]) {
     let readiness = "WAIT";
 
